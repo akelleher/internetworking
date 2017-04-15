eff756a9581568adef9c1119f5e6d97cc2a2f7b7
Alex Kelleher <alexpkelleher@gmail.com>
2017-04-15 16:22:43 -0400

    basic working hostname fetch

---
+++ central.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+import time, os, sys, datetime, csv, urllib2, socket
+
+def fetchData(url):
+    response = urllib2.urlopen(url, timeout=1)
+    cr = csv.reader(response)
+    haveReadFirstRow = False
+    for row in cr:
+        if(haveReadFirstRow):
+            return row
+        haveReadFirstRow = True
+    return "No Data"
+
+def csv_append(file_name, somedata):
+    """Function to append (or write) data to a csv file
+         
+        This function makes sure a USB flash drive is plugged into the node and creates
+        a file in it for the current experiment. NOTE: THIS FUNCTION IS HALTING.
+         
+        Args:
+        file_name: the string name of the file you want to append to
+        somedata: a list of all the data you want to append, formatted [thing1,thing2,etc]
+         
+        Returns:
+        none
+         
+        Edits:
+        CSV: Appends to a CSV file.
+         
+    """
+    with open(file_name, "a") as csvfile:
+        testwriter = csv.writer(csvfile)
+        testwriter.writerow(somedata)
+ 
+    csvfile.close()
+
+urls = ['http://localhost']
+localFile = "test.csv"
+numSuccess = 0
+numFail = 0
+while(True):
+    for url in urls:
+        try:
+            data = fetchData(url)
+            csv_append(localFile, data)
+            numSuccess += 1
+            print("%s -- %d successes and %d failures" %(data, numSuccess, numFail))
+        except socket.timeout as e:
+            numFail += 1
+            print("Failed to connect to %s -- %d success and %d failures" % (url, numSuccess, numFail))
+	except urllib2.URLError as e:
+	    numFail += 1
+            print("Failed to connect to %s -- %d success and %d failures" % (url, numSuccess, numFail))
+    time.sleep(1)
+
+
---
+++ endpoint.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+import time, BaseHTTPServer, os, sys, datetime
+from subprocess import check_output
+
+PORT_NUMBER = 80
+
+#---------------------------------------------------------------------------------------------
+# FUNCTION DEFINITIONS
+#---------------------------------------------------------------------------------------------
+
+class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def do_HEAD(s):
+        s.send_response(200)
+        s.send_header("Content-type", "text/csv")
+        s.end_headers()
+    def do_GET(s):
+        """Respond to a GET request."""
+        s.send_response(200)
+        s.send_header("Content-type", "text/csv")
+        s.end_headers()
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") #Get current timestamp
+        # If someone went to "http://something.somewhere.net/foo/bar/",
+        # then s.path equals "/foo/bar/".
+        s.wfile.write("timestamp,hostname\n%s,%s" %(str(timestamp), hostname))
+        #print ("Temperature is %.2f" % temp)
+
+
+# --------------------------------------------------------------------------
+# MAIN PROGRAM BEGINS HERE
+# --------------------------------------------------------------------------
+
+PORT_NUMBER = 80 # Standard HTTP
+  
+
+
+if __name__ == '__main__':
+
+    hostname = check_output(['cat', '/etc/hostname']) # Pull system hostname
+    hostname = (hostname.decode('utf-8')).strip()   # Convert to text and strip whitespace
+
+    server_class = BaseHTTPServer.HTTPServer
+    httpd = server_class(('', PORT_NUMBER), MyHandler)
+    print time.asctime(), "Server Starts - %s:%s" % (hostname, PORT_NUMBER)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+
+    server_class = BaseHTTPServer.HTTPServer
+    httpd = server_class(('', PORT_NUMBER), MyHandler)
+    print time.asctime(), "Server Starts - %s:%s" % (hostname, PORT_NUMBER)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    print time.asctime(), "Server Stops - %s:%s" % (hostname, PORT_NUMBER)
